fcaed42f4dc506fba9ce741b5cbb6a30c2025b6c
a11ce <a11ce@protonmail.com>
2025-12-13 01:40:19 -0800

    fix stuff

--- objects/campCow.tsx
+++ objects/campCow.tsx
@@ -2,13 +2,16 @@ import { type GameObject } from "@roc/core/gameObject";
 import { Sprite } from "@roc/core/sprite";
 import type { CowCtx, CowState } from "../game";
 
-export function createCampCow(startX: number, state: CowState): GameObject<CowCtx> {
+export function createCampCow(
+  startX: number,
+  state: CowState,
+): GameObject<CowCtx> {
   const getAssetPaths = () => ["cow.png", "cross.png"];
-    const xAfterMoving = 1000;
-    const speed = 2.5;
-    let x = startX;
-    let isMovingRight = false;
-    let hasMoved = false;
+  const xAfterMoving = 1000;
+  const speed = 2.5;
+  let x = startX;
+  let isMovingRight = false;
+  let hasMoved = false;
   const getX = () => x;
 
   const getSprite = (_ctx: CowCtx) => {
@@ -17,27 +20,26 @@ export function createCampCow(startX: number, state: CowState): GameObject<CowCt
         return Sprite.fromFile("cow.png", 0.3).flip();
       case "dead":
         return Sprite.fromFile("cross.png", 0.3);
-      }
- 
+    }
   };
 
-    const onEnterRoom = async (ctx: CowCtx) => {
-        console.log("room entered");
-        if (!hasMoved) {
-            hasMoved = true;
-            isMovingRight = false;
-            await ctx.task.runTask(() => {
-                x += speed;
-                return x <= xAfterMoving;
-            });
-            isMovingRight = false;
-            
-        }
+  const onEnterRoom = async (ctx: CowCtx) => {
+    console.log("room entered");
+    if (!hasMoved) {
+      hasMoved = true;
+      isMovingRight = false;
+      await ctx.task.runTask(() => {
+        x -= speed;
+        return x <= xAfterMoving;
+      });
+      isMovingRight = false;
     }
+  };
 
   return {
     getAssetPaths,
     getX,
     getSprite,
+    onEnterRoom,
   };
 }
--- rooms/camp.tsx
+++ rooms/camp.tsx
@@ -6,15 +6,15 @@ import { avatarSideview } from "../avatars";
 //const bessie = createCampCow(600, ctx.bessieState);
 //const nessie = createCampCow(800, ctx.nessieState);
 
-
 export const camp = createResetRoom<CowCtx>((ctx) => {
-    console.log(ctx);
-    const onEnter = (ctx: CowCtx) => {
-        ctx.avatar.set(avatarSideview);
-        ctx.log.write(`The three most prized cattle of Slickhand Ranch's very own Big Rustlin' Pedro De Las Vacas, each having won Best in Class at this year's Rootinest Tootinest Cow Exhibition are traveling with you, their designated protector, a Cow-Boy. Each of the claims to the various Rootin Tootin titles they held were bona fide, but their personalities were nothing short of difficult. It's been a long fortnight on the dusty trail and the small town of Cactus Sands Ravine can be seen peeking over the horizon. After setting up camp and eating your nightly dinner of Beans and S'mores you're just about ready for bed.`);
+  //console.log(ctx);
+  const onEnter = (ctx: CowCtx) => {
+    ctx.avatar.set(avatarSideview);
+    ctx.log.write(
+      `The three most prized cattle of Slickhand Ranch's very own Big Rustlin' Pedro De Las Vacas, each having won Best in Class at this year's Rootinest Tootinest Cow Exhibition are traveling with you, their designated protector, a Cow-Boy. Each of the claims to the various Rootin Tootin titles they held were bona fide, but their personalities were nothing short of difficult. It's been a long fortnight on the dusty trail and the small town of Cactus Sands Ravine can be seen peeking over the horizon. After setting up camp and eating your nightly dinner of Beans and S'mores you're just about ready for bed.`,
+    );
   };
-    const tessie = createCampCow(1400, ctx.tessieState);
- 
+  const tessie = createCampCow(1400, ctx.tessieState);
 
   return {
     avatarPosition: { x: 100, y: 0 },
